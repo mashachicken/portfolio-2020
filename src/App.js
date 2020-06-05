ed1485bdd91484c85e0375b7986cc3202019cdc2
Masha Ivy <mashanechmo@gmail.com>
2020-06-04 20:42:53 -0700

    initialize a portfolio project, add a few links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ul class="nav-links-left">
+        <li class='list-1'> <Link to='/about' class='nav-link'> About </Link> </li>
+        <li class='list-2'> <a href='/projects' class='nav-link-2'> Projects </a> </li>
+        <li class='list-3'> <a href='/contacts' class='nav-link-3'> Contacts </a> </li>
+      </ul>
+      <h6 class='heading'> Masha Ivy </h6>
+      <ul class='nav-links-right'>
+        <li class='list-4'> L </li>
+      </ul>
+
+
+    </React.Fragment>
   );
 }
 
